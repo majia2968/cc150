1393076ab22c3f48a1cb634fa2d0901236a18f34
luo_x <luo_x@236168LT-DTMSI.ad.psb.bls.gov>
2015-05-05 17:10:38 -0400

    Add stack C3_1

--- src/main/stacks_and_queues/C3_1.java
+++ src/main/stacks_and_queues/C3_1.java
@@ -1,5 +1,54 @@
 package main.stacks_and_queues;
 
 public class C3_1 {
+	
+	int stackSize = 300;
+	int indexUsed = 0;
+	int[] stackPointer = {-1, -1, -1};
+	StackNode[] buffer = new StackNode[stackSize * 3];
+	
+	public void push(int stackNum, int value) {
+		int lastIndex = stackPointer[stackNum];
+		stackPointer[stackNum] = indexUsed;
+		indexUsed++;
+		buffer[stackPointer[stackNum]] = new StackNode(lastIndex, value);
+	}
+	
+	public int pop(int stackNum) {
+		int value = buffer[stackPointer[stackNum]].value;
+		int lastIndex = stackPointer[stackNum];
+		stackPointer[stackNum] = buffer[stackPointer[stackNum]].previous;
+		buffer[lastIndex] = null;
+		indexUsed--;
+		return value;
+	}
+	
+	int peek(int stack) {
+		return buffer[stackPointer[stack]].value;
+	}
+	
+	boolean isEmpty(int stackNum) {
+		return stackPointer[stackNum] == -1;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (StackNode n : buffer) {
+			if (n != null) {
+			sb.append("(" + n.previous + " " + n.value + ")" + "+");
+			}
+			}
+		return sb.toString();
+	}
+	
+	class StackNode {
+		public int previous;
+		public int value;
+		
+		public StackNode(int p, int v) {
+			value = v;
+			previous = p;
+		}
+	}
 
 }
---
+++ src/test/stacks_and_queues/C3_1Test.java
@@ -0,0 +1,22 @@
+package test.stacks_and_queues;
+
+import static org.junit.Assert.*;
+import main.stacks_and_queues.C3_1;
+
+import org.junit.Test;
+
+public class C3_1Test {
+
+	@Test
+	public void test() {
+		C3_1 stack = new C3_1();
+		stack.push(0, 1);
+		stack.push(0, 2);
+		stack.push(0, 3);
+		stack.push(1, 4);
+		stack.push(1, 5);
+		stack.push(1, 6);
+		System.out.println(stack.toString());
+	}
+
+}
